5ac324b53b082a1c7d7430203dee312184185acd
Yess Codes <ycardenas@fitpal.co>
2023-05-23 02:36:19 -0500

    images fix

--- src/app/skills/constants/slides-skill.ts
+++ src/app/skills/constants/slides-skill.ts
@@ -3,42 +3,42 @@ import { ICardSkill } from "src/app/shared/interfaces/card-skill.interface";
 export const CARDS_SKILLS: ICardSkill[] = [
     {
         title: "HTML",
-        img: "../../assets/img/html.svg"
+        img: "./../../assets/img/html.svg"
     },
     {
         title: "CSS",
-        img: "../../assets/img/css.svg"
+        img: "./../../assets/img/css.svg"
     },
     {
         title: "JavaScript",
-        img: "../../assets/img/javascript.svg"
+        img: "./../../assets/img/javascript.svg"
     },
     {
         title: "TypeScript",
-        img: "../../assets/img/typescript.svg"
+        img: "./../../assets/img/typescript.svg"
     },
     {
         title: "Angular",
-        img: "../../assets/img/angular.png"
+        img: "./../../assets/img/angular.png"
     },
     {
         title: "Ionic framework",
-        img: "../../assets/img/ionic.png"
+        img: "./../../assets/img/ionic.png"
     },
     {
         title: "React",
-        img: "../../assets/img/react.png"
+        img: "./../../assets/img/react.png"
     },
     {
         title: "PHP",
-        img: "../../assets/img/php.png"
+        img: "./../../assets/img/php.png"
     },
     {
         title: "Laravel",
-        img: "../../assets/img/laravel.png"
+        img: "./../../assets/img/laravel.png"
     },
     {
         title: "MySql",
-        img: "../../assets/img/mysql.svg"
+        img: "./../../assets/img/mysql.svg"
     },
 ];
